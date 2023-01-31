5dcb1fd45a1886e4ba1746b1909507473ea7f6a4
Theodore John Josef <john.josef@aol.com>
2023-01-31 01:54:42 -0800

    commit tree e556c34a33b3abc6b3ecd3acc894d61a3f246068

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    #byebug
+    student = Student.find(params[:id]) 
+    render json: student 
+  end
 end
